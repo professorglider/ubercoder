2f4ff1765f20c9096c2fd113075bf99715c9897f
viktor <glider@protonmail.com>
2023-03-23 18:36:51 +0400

    json_file is now an argument env variables section added

--- create_project.py
+++ create_project.py
@@ -1,12 +1,12 @@
 import json
 import os
-import subprocess
+import sys
 
 def create_folders(folders, base_path="."):
     for folder in folders:
         folder_path = os.path.join(base_path, folder["name"])
         os.makedirs(folder_path, exist_ok=True)
-        
+
         if "subfolders" in folder:
             create_folders(folder["subfolders"], folder_path)
 
@@ -15,19 +15,24 @@ def create_files(files):
         with open(file["name"], "w") as f:
             f.write(file["content"])
 
-def run_commands(commands):
-    for command in commands:
-        print(f"Executing: {command['description']}")
-        subprocess.run(command["command"], shell=True, check=True)
-
-def create_project_from_json(json_file):
+def main(json_file):
     with open(json_file, "r") as f:
         project_data = json.load(f)
 
+    os.makedirs(project_data["project_name"], exist_ok=True)
+    os.chdir(project_data["project_name"])
+
     create_folders(project_data["folders"])
     create_files(project_data["files"])
-    run_commands(project_data["commands"])
+
+    if "env_file" in project_data:
+        with open(project_data["env_file"]["name"], "w") as f:
+            f.write(project_data["env_file"]["content"])
 
 if __name__ == "__main__":
-    json_file = "example_project.json"  # Replace this with the path to your JSON file
-    create_project_from_json(json_file)
+    if len(sys.argv) < 2:
+        print("Usage: python create_project.py <json_file>")
+        sys.exit(1)
+
+    json_file = sys.argv[1]
+    main(json_file)
